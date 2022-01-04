2434cd79bbce7b62d985a13a5ccf53bce05da2cf
David Klinke <45146596+KlinkeLab@users.noreply.github.com>
2022-01-04 10:51:03 -0500

    Update 4_Merge_Datasets.R
    added GPL license info

--- 4_Merge_Datasets.R
+++ 4_Merge_Datasets.R
@@ -1,3 +1,19 @@
+# 4_Merge_Datasets.R - merges different digital cytometry measures and sample features together
+# Copyright (C) 2022 Klinke Lab
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # This file needs to merge the different digital cytometry measures together
 # Create a "Cancer" column with 1 if the sample was cancer and 0 if normal
 # create pM0, pM1, and pM2 metrics
