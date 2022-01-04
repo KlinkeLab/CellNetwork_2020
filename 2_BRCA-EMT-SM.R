15ee211a56e394c534ae5f84afb7e97d7b7a3721
David Klinke <45146596+KlinkeLab@users.noreply.github.com>
2022-01-04 10:45:33 -0500

    Update 2_BRCA-EMT-SM.R
    Added GPL license info

--- 2_BRCA-EMT-SM.R
+++ 2_BRCA-EMT-SM.R
@@ -1,3 +1,19 @@
+# 2_BRCA-EMT-SM.R - calculates values for Epithelial and Mesenchymal state metrics
+# Copyright (C) 2022 Klinke Lab
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # Calculate values for Epithelial and Mesenchymal state metrics
 setwd("E:/Lab Files/Bayes Network/CellNetwork_2020-master")
 rm(list = ls())
